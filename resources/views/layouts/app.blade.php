feeaa8e114543350213aab5da0229cbb28bca8e0
Drago Stevanovic <drago.s@vivifyideas.com>
2020-09-07 19:43:16 +0200

    Move create a post link to the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,11 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <!-- Left Side Of Navbar -->
           <ul class="navbar-nav mr-auto">
-
+            @auth
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('createPostForm')}}"> Create a post </a>
+              </li>
+            @endauth
           </ul>
 
           <!-- Right Side Of Navbar -->
--- resources/views/posts/all.blade.php
+++ resources/views/posts/all.blade.php
@@ -4,9 +4,6 @@
 
 @section('content')
   <h1>My posts</h1>
-  <a class="btn btn-primary" href="{{route('createPostForm')}}">
-    Create a post
-  </a>
   @foreach ($posts as $post)
     <div>
       <a href="{{route('singlePost', ['id' => $post->id])}}">{{$post->title}} ({{$post->comments->count()}})</a>
